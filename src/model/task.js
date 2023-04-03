cd72bf86b3d0d1ae251632e2b774bf843a56bbf4
F0r4st3r0 <pjmssb@gmail.com>
2023-04-03 19:02:41 +0100

    Inicio del modelo

---
+++ src/model/task.js
@@ -0,0 +1,32 @@
+const { v4: uuidv4 } = require('uuid');
+const db = require('TaskRepository');
+ const log = require('TaskLoggingService');
+
+class Task {
+  constructor({title, description = "", due = null, status = "pending"}) {
+    this.id = uuidv4();
+    this.title = String(title).trim();
+    this.description = String(description).trim();
+    this.dueDate = due instanceof Date ? due : null;
+    this.status = ["pending", "completed", "deleted", "invalid"].includes(status) ? status : "pending";
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+    if (!this.isValid()) {
+
+    }
+    
+  }
+
+  function isValid() {
+    if ((this.createdAt > this.dueDate) || 
+        (this.createdAt > this.updatedAt) ||
+        (this.updatedAt > this.dueDate ) ||
+        (this.dueDate == null) ||
+        (this.descriprion.length > 0)) {
+            log.event('Esta tarea no es válida');
+            return false; 
+    };
+    return true;
+  }
+
+}
