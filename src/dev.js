9943f9ff3f8aac292724e1f1f96d85fb5ed66cc0
F0r4st3r0 <pjmssb@gmail.com>
2023-04-05 15:16:15 +0100

    Probando commits desde VSCode

--- src/dev.js
+++ src/dev.js
@@ -1,2 +1,21 @@
-const task = require('./model/task.js');
+const { clear } = require('console');
+const Task = require('./model/task.js');
 
+const createTask = async () => {  
+  const title = 'Test Title';
+  const description = 'Description test. Lorem ipsum dolor sic amet';
+  const due = '2023-06-02';
+  const testTaskObject = {title, description, due};
+
+  try {
+    const task = new Task(testTaskObject);
+    await task.ready();    
+    console.log(`Task "${task.title}" created with ID ${task.id}`);
+  } catch(err) {    
+	  console.error('Error ++++++++++++++++++++');
+    console.error(err.message)
+  }
+  
+};
+
+createTask();
--- src/model/task.js
+++ src/model/task.js
@@ -1,4 +1,18 @@
 const { v4: uuidv4 } = require('uuid');
 
-module.exports = class Task {
+class Task {
+  constructor({title, description = "", due = null}) {
+    this.id = uuidv4();
+    this.title = String(title).trim();
+    this.description = String(description).trim();
+    this.dueDate = due instanceof Date ? due : null;
+    this.status = "pending";
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+  }
 }
+
+
+
+
+module.exports = Task;
